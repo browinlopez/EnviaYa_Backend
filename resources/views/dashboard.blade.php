7c78ed46ff737a89b05ee42c243f88b08dd54d89
browinlopez <browinsantiago@gmail.com>
2025-09-12 23:49:00 -0500

    Mejoras: 1)Creacion Dashboard ADMIN 12/09/2025 - 5:10PM

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -146,7 +146,7 @@
                                     <div>
                                         <strong>{{ $product->name }}</strong><br>
                                         <small class="text-muted">
-                                            Negocio: {{ $product->businesses->name ?? 'Sin negocio' }}
+                                            Negocio: {{ $product->businesses->first()->name ?? 'Sin negocio' }}
                                         </small>
                                     </div>
                                     <span class="badge badge-success badge-pill">
