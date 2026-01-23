a171e58767fae9a8ab37492b5d8383ff3d1941a3
browinlopez <browinsantiago@gmail.com>
2026-01-23 18:15:44 -0500

    Mejoras: 1)Modificaciones Categorias de negocios 2)Api

--- app/Http/Controllers/Review/ReviewController.php
+++ app/Http/Controllers/Review/ReviewController.php
@@ -130,7 +130,7 @@ class ReviewController extends Controller
     public function listReviewsByBusiness(Request $request)
     {
         $request->validate([
-            'busines_id' => 'required|integer|exists:business,busines_id',
+            'business_id' => 'required|integer|exists:business,busines_id',
         ]);
 
         $reviews = BusinessReview::with('business', 'buyer.user') // <-- aquí el cambio
